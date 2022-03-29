841dc496be6fc6f3863cd884ba7999a9d3608b40
Haonan Chang <chnme40cs@gmail.com>
2022-03-29 15:44:46 -0400

    Update: change some spaces

--- generate.js
+++ generate.js
@@ -4,21 +4,6 @@ const config = require('./config')
 
 const compiledFunction = pug.compileFile('./views/index.pug');
 
-// let config = {
-//     repository_name: '/Easy3DViewer',
-//     data: [{
-//     name:"move_dragon",
-//     folder_name: "move_dragon4Cam", 
-//     title: "Move Dragon Toy",
-//     frames: ["frame_000001", "frame_000002", "frame_000003", "frame_000004","frame_000005"]
-//     },{
-//         name:"scarf",
-//         folder_name: "scarf4Cam", 
-//         title: "Moving Scarf",
-//         frames: ["frame_000001"]
-//     }]
-// }
-
 fs.writeFile('index.html', compiledFunction({
     git_root_file: config.repository_name, default_data_folder:config.default_data_folder, config: config.data
     }), function (err) {
