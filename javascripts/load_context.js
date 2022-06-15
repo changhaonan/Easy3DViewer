efcee48e3d049ad4a54f01d29da78ad7c67d43ac
Vatsal Shah <56099342+vatsalbshah99@users.noreply.github.com>
2022-06-15 00:02:00 -0400

    Update load_context.js

--- javascripts/load_context.js
+++ javascripts/load_context.js
@@ -88,7 +88,7 @@ function parseJson(file_path, engine_data, loadModel) {
         $.each(engine_data.vis_controls, (key) => {
             if (engine_data.controls[key].getValue()) {
                 $.each(engine_data.vis_controls[key],
-                    (_i, name) => {
+                    function (_i, name) {
                         loadModel(name, engine_data.data[name], engine_data);
                         engine_data.obj_loaded.push(name);
                     })
