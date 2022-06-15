49a53b52ee3ebb344acbea9d68ce18c3db68f895
Vatsal Shah <56099342+vatsalbshah99@users.noreply.github.com>
2022-06-14 22:18:13 -0400

    Update load_context.js

--- javascripts/load_context.js
+++ javascripts/load_context.js
@@ -89,8 +89,10 @@ function parseJson(file_path, engine_data, loadModel) {
             if (engine_data.controls[key].getValue()) {
                 $.each(engine_data.vis_controls[key],
                     (_i, name) => {
-                        loadModel(name, engine_data.data[name], engine_data);
+                        //loadModel(name, engine_data.data[name], engine_data);
                         engine_data.obj_loaded.push(name);
+                        console.log(name);
+                        console.log(engine_data.obj_loaded);
                     })
             }
         });
