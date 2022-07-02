fbd683d38b3d3dfec4225e962c745b86e669a148
Haonan Chang <chnme40cs@gmail.com>
2022-07-02 16:25:51 -0700

    update: increase the robustness of system

--- configure.py
+++ configure.py
@@ -13,13 +13,14 @@ if __name__ == "__main__":
     config_json["data"] = list()
 
     for dir_name in os.listdir(os.path.join("public", data_dir)):
-        canvas_json = dict()
-        canvas_json["source_directory"] = f"/{data_dir}"
-        canvas_json["name"] = dir_name
-        canvas_json["title"] = dir_name   
-        canvas_json["folder_name"] = dir_name   
-        canvas_json["frames"] = os.listdir(os.path.join("public", data_dir, dir_name))
-        config_json["data"].append(canvas_json)
+        if os.path.isdir(os.path.join("public", data_dir, dir_name)):
+            canvas_json = dict()
+            canvas_json["source_directory"] = f"/{data_dir}"
+            canvas_json["name"] = dir_name
+            canvas_json["title"] = dir_name   
+            canvas_json["folder_name"] = dir_name   
+            canvas_json["frames"] = os.listdir(os.path.join("public", data_dir, dir_name))
+            config_json["data"].append(canvas_json)
 
     with open("config.json", "w") as f:
         json.dump(config_json, f)
