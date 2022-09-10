b8cbf7a677574fc8548721a9f4642d7a1d23098b
Harvey Chang <chnme40cs@gmail.com>
2022-09-09 20:42:42 -0400

    update: make adjustment to suit STAR

--- configure.py
+++ configure.py
@@ -1,12 +1,21 @@
 """ Generate default config.json based on 
 """
+from genericpath import isdir
 import json
 import os
+import argparse
 
 if __name__ == "__main__":
+    # 1. Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--data_dir", type=str, default="test_data", help="data directory")
+    parser.add_argument("-r", "--repository_name", type=str, default="http://localhost:8000", help="directory to publish")
+    args = parser.parse_args()
+
+    # 2. Generate config.json
     config_json = dict()
-    data_dir = "test_data"
-    repository_name = "http://localhost:8000"
+    data_dir = args.data_dir
+    repository_name = args.repository_name
 
     config_json["repository_name"] = repository_name
     config_json["default_data_folder"] = f"/{data_dir}"
@@ -18,8 +27,12 @@ if __name__ == "__main__":
             canvas_json["source_directory"] = f"/{data_dir}"
             canvas_json["name"] = dir_name
             canvas_json["title"] = dir_name   
-            canvas_json["folder_name"] = dir_name   
-            canvas_json["frames"] = os.listdir(os.path.join("public", data_dir, dir_name))
+            canvas_json["folder_name"] = dir_name
+            frame_list = list()
+            for file_name in os.listdir(os.path.join("public", data_dir, dir_name)):
+                if file_name.startswith("frame"):
+                    frame_list.append(file_name)
+            canvas_json["frames"] = frame_list
             config_json["data"].append(canvas_json)
 
     with open("config.json", "w") as f:
